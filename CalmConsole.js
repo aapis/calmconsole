af12341f9508fcd37bd07dd4b143b3fa6791a9a6
Ryan Priebe <rpriebe@me.com>
2012-12-02 23:28:52 -0700

    Some documentation, more styles, new event triggers and refactored constructor

--- CalmConsole.js
+++ CalmConsole.js
@@ -1,26 +1,29 @@
 //TODO:
-//- extend window.console so any regular console logs show up [IN PROGRESS]
-//- compatibility with IE
+//- extend window.console so any regular console logs show up
 //- push all logged messages to __actions (or something) and parse/display later to cut down on calls to .appendChild()
 //- preserve login upon navigation (localstorage)
+//- add documentation
 var CalmConsole = function(options){
 	'use strict';
 
-	var CalmObj = this;
-	var RenderedObj = null;
-	var ActionList = null;
-	var Toggle = null;
+	var CalmObj = this,
+		RenderedObj = null,
+		ActionList = null,
+		Toggle = null,
+		Close = null,
+		__actions = [],
+		options = (!options ? {} : options);;
 
 	/*
 	 * ---------------------------------------------------------------------------------
 	 * Publicly accessible methods
 	 * ---------------------------------------------------------------------------------
 	 */
-	this.__init__ = function(options){
+	this.__init__ = function(){
 		if(window.console){
 			console.warn('Your browser supports the Console, but CalmConsole won\'t affect it\'s functionality.');
 		}
-
+		
 		//create the UI
 		_loadUI();
 		_loadListeners();
@@ -64,21 +67,36 @@ var CalmConsole = function(options){
 	 */
 	
 	function _loadUI(){
+		//Create the console
 		var consoleElement = document.createElement('div');
 			consoleElement.classList.add('CalmConsole');
 
+		//Create the minimize/maximize toggle element
 		var toggleElement = document.createElement('a');
 			toggleElement.classList.add('toggle');
 			toggleElement.innerHTML = 'Minimize';
 			toggleElement.href = '#';
 
+		//Create the close console element
+		var closeElement = document.createElement('a');
+			closeElement.classList.add('close');
+			closeElement.innerHTML = 'Close';
+			closeElement.href = '#';
+
+		//Create the console header element
 		var headerElement = document.createElement('header');
 			headerElement.classList.add('header');
 			headerElement.innerHTML = '<h2>CalmConsole</h2>';
 
+		//Create the wrapper for the minimize/close controls
+		var controlElement = document.createElement('div');
+			controlElement.classList.add('controls');
+
+		//Create the console log list element
 		var listElement = document.createElement('ul');
 			listElement.classList.add('msg-list');
 
+		Close = closeElement;
 		ActionList = listElement;
 		RenderedObj = consoleElement;
 		Toggle = toggleElement;
@@ -87,7 +105,9 @@ var CalmConsole = function(options){
 		document.body.appendChild(RenderedObj);
 
 		//add the various UI elements too
-		headerElement.appendChild(toggleElement);
+		headerElement.appendChild(controlElement);
+		controlElement.appendChild(toggleElement);
+		controlElement.appendChild(closeElement);
 		RenderedObj.appendChild(headerElement);
 		RenderedObj.appendChild(ActionList);
 
@@ -113,7 +133,7 @@ var CalmConsole = function(options){
 	};
 
 	function _outputObject(toLog, classes){
-		//build an object, use it as return value
+		//TODO: build an object, use it as return value
 		return toLog;
 	};
 
@@ -124,33 +144,67 @@ var CalmConsole = function(options){
 			ActionList.appendChild(_outputObject(toLog, classes));
 		}
 
+		//__actions.push(_outputString(toLog, classes));
+		//console.log(__actions);
+
 		return null;
 	};
 
 	function _loadStyles(){
 		var stylesheet = document.createElement('style');
-			stylesheet.innerHTML = '.CalmConsole {position: fixed; bottom: 0px; width: 100%; height: 300px; color: black; overflow: scroll; background: white; border-top: 1px solid rgba(0,0,0,0.3); font-family: "Lucida Sans Unicode";} .CalmConsole li {padding: 3px; margin: 0px; border-bottom: 1px solid rgba(0,0,0,0.3)} .CalmConsole .toggle {position: absolute; right: 10px; top: 30%;} .CalmConsole.minimized {height: 41px;} .CalmConsole.minimized .toggle {background-color: rgba(0,0,0,1);} .CalmConsole .msg-warning {background-color: #FCF8E3;} .CalmConsole .msg-special {background-color: #D9EDF7;} .CalmConsole .msg-error {background-color: #F2DEDE;} .CalmConsole .msg-success {background-color: #DFF0D8;} .CalmConsole ul {padding: 0px; margin: 0px;} .CalmConsole header {position: relative; font-family: Helvetica, Arial, sans-serif; border-bottom: 1px solid rgba(0,0,0,0.3); background-image: -ms-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: -moz-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: -o-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #FFFFFF), color-stop(1, #EEEEEE)); background-image: -webkit-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: linear-gradient(to bottom, #FFFFFF 0%, #EEEEEE 100%);} .CalmConsole header h2 {float: left; margin: 10px;} .CalmConsole header, .CalmConsole ul.msg-list {float: left; width: 100%; font-size: 11px;} .CalmConsole a {color: white; background: rgba(0,0,0,0.4); border-radius: 4px; padding: 3px; text-decoration: none;}';
+			stylesheet.innerHTML = '.CalmConsole {position: fixed; bottom: 0px; width: 100%; height: 300px; font-size: 1em; color: black; overflow: scroll; background: white; border-top: 1px solid rgba(0,0,0,0.3); font-family: "Lucida Sans Unicode";} .CalmConsole li {padding: 3px; margin: 0px; border-bottom: 1px solid rgba(0,0,0,0.3)} .CalmConsole .controls {position: absolute; right: 10px; top: 30%;} .CalmConsole.minimized {height: 41px;} .CalmConsole.minimized .toggle {background: rgba(0,0,0,0.1);} .CalmConsole .msg-warning {background-color: #FCF8E3;} .CalmConsole .msg-special {background-color: #D9EDF7;} .CalmConsole .msg-error {background-color: #F2DEDE;} .CalmConsole .msg-success {background-color: #DFF0D8;} .CalmConsole ul {padding: 0px; margin: 0px;} .CalmConsole header {position: relative; font-family: Helvetica, Arial, sans-serif; border-bottom: 1px solid rgba(0,0,0,0.3); background-image: -ms-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: -moz-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: -o-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0, #FFFFFF), color-stop(1, #EEEEEE)); background-image: -webkit-linear-gradient(top, #FFFFFF 0%, #EEEEEE 100%); background-image: linear-gradient(to bottom, #FFFFFF 0%, #EEEEEE 100%);} .CalmConsole header h2 {font-size: 1.5em; float: left; margin: 10px;} .CalmConsole header, .CalmConsole ul.msg-list {float: left; width: 100%; font-size: 11px;} .CalmConsole .controls a {position: relative; top: 1px; color: black; margin-right: 1px; padding: 14px 13px; text-decoration: none;} .CalmConsole.hidden {display: none;}';
 
 		return document.head.appendChild(stylesheet);
 	};
 
 	function _loadListeners(){
-		//var toggle_action_obj = document.querySelector('.CalmConsole .toggle');
-			Toggle.addEventListener('click', function(evt){
-				evt.preventDefault();
+		Toggle.addEventListener('click', function(evt){
+			evt.preventDefault();
 
-				if(RenderedObj.classList.contains('minimized')){
-					RenderedObj.classList.remove('minimized');
-					Toggle.innerHTML = 'Minimize';
-				}else {
-					RenderedObj.classList.add('minimized');
-					Toggle.innerHTML = 'Maximize';
-				}
-			});
+			if(RenderedObj.classList.contains('minimized')){
+				RenderedObj.classList.remove('minimized');
+				Toggle.innerHTML = 'Minimize';
+
+				_setCookie('CalmConsole.toggle', 0);
+			}else {
+				RenderedObj.classList.add('minimized');
+				Toggle.innerHTML = 'Maximize';
+
+				_setCookie('CalmConsole.toggle', 1);
+			}
+		});
+
+		Close.addEventListener('click', function(evt){
+			evt.preventDefault();
+
+			if(options.removeOnClose){
+				RenderedObj.parentElement.removeChild(RenderedObj);
+				_setCookie('CalmConsole.close', 0);
+			}else {
+				RenderedObj.classList.add('hidden');
+				_setCookie('CalmConsole.close', 1);
+			}
+		});
+	};
+
+	function _setCookie(name, value){
+		var date = new Date();
+		date.setDate(date.getDate() + 7);
+
+		var value = escape(value) + "; expires=" + date.toUTCString();
+
+		document.cookie = name + "=" + value;
+	};
+
+	function _getCookie(){
+
 	}
+
+	return this.__init__();
 };
 
-var calm = new CalmConsole;
-	calm.__init__();
+var calm = new CalmConsole({
+		removeOnClose: false,
+	});
 
 
